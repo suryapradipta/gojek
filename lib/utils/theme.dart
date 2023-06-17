f8e0e4ad7b73ce2661ec2f044d2561dfaad2470f
suryapradipta.github.io <63577853+suryapradipta@users.noreply.github.com>
2023-06-18 01:34:43 +0800

    header

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
+// Colors
+Color green1 = const Color(0xff097210);
+Color green2 = const Color(0xff00880F);
+
+Color dark1 = const Color(0xff1C1C1C);
+Color dark2 = const Color(0xff4A4A4A);
+Color dark3 = const Color(0xff999798);
+Color dark4 = const Color(0xffEDEDED);
+
+Color blue1 = const Color(0xff0281A0);
+Color blue2 = const Color(0xff00AED5);
+Color blue3 = const Color(0xff38BBDA);
+
+Color red = const Color(0xffED2739);
+Color purple = const Color(0xff87027B);
+
 // Typography
 TextStyle regular12_5 =
     const TextStyle(fontFamily: 'SF-Pro-Display', fontSize: 12.5);
